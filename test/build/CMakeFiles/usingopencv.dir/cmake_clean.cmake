3a278cbfe04ac83cc91c520a9f18031fff5d5c3b
ben <benliu98230@gmail.com>
2020-06-03 17:30:33 +0800

    This is using opencv to get one color frame and display

---
+++ test/build/CMakeFiles/usingopencv.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/usingopencv.dir/main.cpp.o"
+  "usingopencv.pdb"
+  "usingopencv"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/usingopencv.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
